9db9e629e50f7e4403e83dc8bf244401999a386e
Hasanth <hasanth1993@gmail.com>
2020-11-22 20:08:42 -0500

    Initial commit

---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './main.css';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className="about">
+        <div className="about-text ">About Me<hr/></div>
+        <div>
+            I am having 2.5 years of experience working in Information Technology industry. I am working as a Software Developer in waveXchange implementing web application using MEAN stack and AWS. I have worked at ARC International during Summer 2018 as an IT Intern implementing web applications using Symfony, Java and data warehousing using SQL Data Tools, Power BI and Microsoft products. My previous work experience was as a Systems Engineer at Infosys Limited, developing products and providing services as a full stack web developer using Java Enterprise Edition and worked on Business Intelligence tools in SAP BW and BEx Explorer.    
+            Currently, I am a master's student in Computer Science at The University of Texas at Dallas and looking for full time opportunities involving new career challenges.
+            My specialties include MEAN Stack, Python, Java Enterprise Edition(Hibernate, Spring) and Symfony.
+        </div>
+      </div>
+    )
+  }
+} 
---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './main.css';
+
+export default class Header extends Component {
+
+
+  render() {
+
+    return (
+        <React.Fragment>
+        <div class="navbar-header">
+        
+            <a className="navbar-brand">Hasanth Sista</a>
+            
+            <a className="navbar-linkedin" alt="Linked In" href="https://www.linkedin.com/in/hasanthsista"></a> 
+            
+            <a href="https://github.com/Hasanth-Sista" className="navbar-github" alt="Github"></a>
+
+            <a href="mailto:hasanthsista1993@gmail.com"><button className="navbar-contact">Contact Me</button></a>
+            
+            
+            <a href="Resume.pdf" target="_blank" download><button className="navbar-contact">Resume</button></a>
+            
+        </div>
+        <div className="header">
+            <img src={require('./sista.jpg')} alt="Hasanth Sista"></img>
+            <div class="intro-text">
+                <span class="name">Hasanth Sista</span>
+                <span class="skills">Software Engineer at The Home Depot, Atlanta</span>
+            </div>
+        </div>
+        </React.Fragment>
+    )
+  }
+}
